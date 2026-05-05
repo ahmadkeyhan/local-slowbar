0c7e9ccd880710fda0701564eda314a40b75e2d5
keyhansa <keyhan.ahmad@gmail.com>
2026-05-05 23:45:03 +0330

    omit map for now

--- components/ui/footer.tsx
+++ components/ui/footer.tsx
@@ -10,7 +10,7 @@ export default function Footer() {
     
     return (
         <div className="w-full grid gap-4 sm:grid-cols-2 bg-indigo p-4 pt-6">
-            <div className="flex flex-col gap-2 items-center text-white">
+            <div className="flex flex-col gap-2 items-center">
                 <Link href="/" className="flex flex-col gap-2 items-center">
                     <div className="w-48">
                         <Image
@@ -29,12 +29,9 @@ export default function Footer() {
                     </Button>
                 </Link>
             </div>
-            <div className="flex flex-col gap-2">
+            {/* <div className="flex flex-col gap-2">
                 <div className="overflow-hidden h-full max-w-full rounded-md">
                     <div id="canvas-for-googlemap" className="h-full w-full max-w-full">
-                        {/* <iframe className="h-full w-full border-0" 
-                            src={`https://www.google.com/maps/embed/v1/place?key=${googleMapsApiKey}&q=Cafe+QQ+/قوشاقاف&key=AIzaSyBFw0Qbyq9zTFTd-tUY6dZWTgaQzuU17R8`}>
-                        </iframe> */}
                         <iframe className="h-full w-full border-0"  src={`https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3141.34705589769!2d46.35301977536402!3d38.06229789554638!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x401a1bd67ea97281%3A0xc90002f1232b4cc8!2sLido%20Cafe!5e0!3m2!1sen!2s!4v1770990764476!5m2!1sen!2s`}  allowFullScreen={true} loading="lazy" referrerPolicy="no-referrer-when-downgrade"></iframe>
                     </div>
                 </div>
@@ -42,7 +39,7 @@ export default function Footer() {
                     <span><LuMapPin className="w-4 h-4 inline ml-1" /></span>
                     تبریز/ ولیعصر/ خیابان فروغی/ خیابان قطران
                 </p>
-            </div>
+            </div> */}
             <div className="flex sm:col-span-2 justify-center items-center gap-2 text-sm text-peach py-2">
                 <LuLaptop className="w-4 h-4" />
                 <p className="text-sm">طراحی و توسعه توسط <Link href="https://www.instagram.com/key.hansa" target="_blank" rel="noopener noreferrer"><span className="border-b font-bold">کیهانزا</span></Link></p>
